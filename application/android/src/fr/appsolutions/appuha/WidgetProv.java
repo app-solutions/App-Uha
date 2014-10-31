6a4a98167c1b1f33fa8c0c00137ada7e88a88255
Clément Mouline <clement.mouline@app-solutions.fr>
2014-10-31 23:04:05 +0100

    Uptate JsonReader

--- application/android/src/fr/appsolutions/appuha/WidgetProv.java
+++ application/android/src/fr/appsolutions/appuha/WidgetProv.java
@@ -16,14 +16,14 @@ import fr.appsolutions.appuha.utilitaires.Utilitaires;
 
 public class WidgetProv extends AppWidgetProvider {
 
-	
+
 	private Calendar calendar = Calendar.getInstance();
 	int day_name = calendar.get(Calendar.DAY_OF_WEEK);
 	int hour = calendar.get(Calendar.HOUR_OF_DAY);
 	int minutes = calendar.get(Calendar.MINUTE);
 	String timestamp_sting = hour + "H" + minutes;
-	
-	
+
+
 	@SuppressWarnings("static-access")
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -32,16 +32,14 @@ public class WidgetProv extends AppWidgetProvider {
 		Log.i(Utilitaires.TAG, "Widget update receive");
 		final int N = appWidgetIds.length;
 
-		
-		
 		for (int i = 0; i < N; i++) {
-				
+
 			try{
-				
+
 				String planning = (String) context.getSharedPreferences(LOCAL_PLANNING_NAME, context.MODE_PRIVATE).getString("planning", null);
 				JsonReader jsr = new JsonReader(planning);
 				ArrayList<Cours> cours = null;
-				
+
 				switch(day_name){
 				case Calendar.MONDAY:
 					cours = jsr.getDayCours(JsonReader.S1, JsonReader.LUNDI);
@@ -65,30 +63,31 @@ public class WidgetProv extends AppWidgetProvider {
 					cours = jsr.getDayCours(JsonReader.S1, JsonReader.LUNDI);
 					break;
 				}
-				
-				
+
+
 				int current_timestamp = Integer.valueOf(timestamp_sting.replace("H", ""));
 				Cours nextCours = null;
-				
+
 				for(int j=0; j<cours.size(); j++){
 					if(current_timestamp < Integer.valueOf(cours.get(j).getStart().replace("H", ""))){
 						nextCours = cours.get(j);
 						break;
 					}
 				}
-				
-				
+
+
 				// update layout
 				RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
 				views.setTextViewText(R.id.textCoursName, "Prochain cours: " + nextCours.getNom());
 				views.setTextViewText(R.id.textCoursStart,"à: " + nextCours.getStart());
 				views.setTextViewText(R.id.textCoursRoom, "salle: " + nextCours.getSalle());
 				appWidgetManager.updateAppWidget(appWidgetIds[i], views);
-				
+
 			} catch (Exception e){
 				e.printStackTrace();
+				Log.e(Utilitaires.TAG, e.toString());
 			}
-			
+
 		}
 
 	}
--- application/android/src/fr/appsolutions/appuha/jsonreader/JsonReader.java
+++ application/android/src/fr/appsolutions/appuha/jsonreader/JsonReader.java
@@ -10,119 +10,188 @@ import org.json.JSONTokener;
 import fr.appsolutions.appuha.autres.Cours;
 
 public class JsonReader {
-	
+
 	private JSONObject appUha;
-	
+
 	public static final String S1 = "semaine1";
 	public static final String S2 = "semaine2";
 	public static final String S3 = "semaine3";
 	public static final String S4 = "semaine4";
-	
+
 	public static final String LUNDI = "lundi";
 	public static final String MARDI = "mardi";
 	public static final String MERCREDI = "mercredi";
 	public static final String JEUDI = "jeudi";
 	public static final String VENDREDI = "vendredi";
 	public static final String SAMEDI = "samedi";
-	
+
 	public JsonReader(String json) throws JSONException{
 		appUha = (JSONObject) new JSONTokener(json).nextValue();
 		appUha = appUha.getJSONObject("AppUha");
 	}
-	
+
 	/**
 	 * les codes ci-dessous, sont des codes de retour génériques, donnés pour toutes requettes serveur.
 	 * 
 	 */
-	
+
 	//code de retour de la requette
 	public String getResultCode() throws JSONException{
 		return (String) appUha.get("resultCode");
 	}
-	
+
 	//header de la requette
 	public String getResultHeader() throws JSONException{
 		return (String) appUha.get("resultHeader");
 	}
-	
+
 	//titre de la requette
 	public String getResultTitle() throws JSONException{
 		return (String) appUha.getString("resultTitle");
 	}
-	
+
 	//String de le la requette
 	public String getResultString() throws JSONException{
 		return (String) appUha.get("resultString");
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Les requettes ci-dessous sont des requettes utilisées pour recuperer les elements du Service GetUserInfo
 	 * 
 	 */
-	
+
 	//retourne le prenom
 	public String getUserInfoPrenom() throws JSONException{
 		return (String) appUha.getJSONObject("infos").get("prenom");
 	}
-	
+
 	//retourne le nom
 	public String getUserInfoNom() throws JSONException{
 		return (String) appUha.getJSONObject("infos").get("nom");
 	}
-	
+
 	//retourne l'email
 	public String getUserInfoEmail() throws JSONException{
 		return (String) appUha.getJSONObject("infos").get("email");
-		
+
 	}
-	
+
 	//retourne le groupe de la personne
 	public String getUserInfoGroupe() throws JSONException{
 		return (String) appUha.getJSONObject("infos").get("groupe");
 	}
-	
+
 	//retourne le nombre de notification restante 
 	public String getUserInfoNotif() throws JSONException{
 		return (String) appUha.getJSONObject("infos").get("notifs");
 	}
-	
-	
+
+
 	/**
 	 * Les requettes suivantes, sont utilisées pour recuperer les cours d'une semaine du service GetPlanning
 	 */
-	
+
 	//retourne le nom d'une semaine
 	public String getWeekName(String semaine) throws JSONException{
 		return (String) appUha.getJSONObject("semaines").getJSONObject(semaine).get("nom");
 	}
-	
-	
+
+
 	//retourne la date d'un jour
 	public String getDayName(String semaine, String jour) throws JSONException{
 		return (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(jour).get("nom");
 	}
-	
-	
+
+
 	//retourne les cours d'un jour
 	public ArrayList<Cours> getDayCours(String semaine, String jour) throws JSONException{
-		
+
 		JSONArray jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(jour).getString("cours") );
-		
+
 
 		ArrayList<Cours> ar = new ArrayList<Cours>();
-		
+
 		for(int i=0; i<jsra.length(); i++){
 			JSONObject j = jsra.getJSONObject(i);
 			ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
 		}
-		
+
 		return ar;
 	}
-	
+
+	//retourne tous les cours
+	public ArrayList<Cours> getAllWeeknDaysCours() throws JSONException{
+
+		ArrayList<Cours> ar = new ArrayList<Cours>();
+
+		for(int i=0; i<4; i++){
+
+			String semaine = null;
+
+			switch(i){
+			case 0:
+				semaine = S1;
+				break;
+			case 1:
+				semaine = S2;
+				break;
+			case 2:
+				semaine = S3;
+				break;
+			case 3:
+				semaine = S4;
+				break;
+
+			}
+
+
+			JSONArray jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(LUNDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+			jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(MARDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+			jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(MERCREDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+			jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(JEUDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+			jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(VENDREDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+			jsra = new JSONArray( (String) appUha.getJSONObject("semaines").getJSONObject(semaine).getJSONObject("jours").getJSONObject(SAMEDI).getString("cours") );		
+			for(int o=0; o<jsra.length(); o++){
+				JSONObject j = jsra.getJSONObject(o);
+				ar.add(new Cours( (String) j.getString("id"), (String) j.getString("start"), (String) j.getString("stop"), (String) j.getString("durée"), (String) j.getString("nom"), (String) j.getString("salle"), (String) j.getString("enseignant")));
+			}
+
+
+
+		}
+
+		return ar;
+	}
+
 	public String test(String jour) throws JSONException{
 		return (String) appUha.getJSONObject("semaines").getJSONObject("semaine1").getJSONObject("jours").getJSONObject(jour).getString("cours");
 	}
-	
+
 }
